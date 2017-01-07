cc130addfdc876748a1d258c99db1dc5c67cf946
Elizabeth Engelman <emengelman@gmail.com>
2017-01-07 13:51:21 -0600

    Uses elizabethengelman-photographs app to get photos

--- javascripts/photos.js
+++ javascripts/photos.js
@@ -1,13 +1,16 @@
 function fetchPhotos() {
-  var flickerAPI = "https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=30c4f326d01f4cb98266aef9d5dd3fe7&format=json&jsoncallback=?&user_id=26430465@N02";
+  var flickerAPI = "https://elizabethengelman-photographs.herokuapp.com/flickr_photos"
 
   $.getJSON(flickerAPI)
-      .done(function(data) {
-        $.each( data.photos.photo, function(i, item) {
-          var imageUrl = "https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" + item.id + "_" + item.secret + ".jpg"
-          $("<img>").attr("src", imageUrl).appendTo("#gallery");
-        });
+    .done(function(data) {
+      $.each( data, function(i, item) {
+        var html = "<div class='box'>" +
+                     "<div class='boxInner'>" +
+                       "<img src='" + item + "'>" +
+                       "</div></div>"
+        $(html).appendTo("#gallery");
       });
+    });
 }
 
 fetchPhotos();
